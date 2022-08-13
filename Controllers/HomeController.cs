86bfbc4aac1e631490167de4b6c13f6d89189014
Dr-Smiles <81245710+Dr-Smiles@users.noreply.github.com>
2022-08-13 21:29:42 +0300

    Template Installed!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebProje2.Models;
+
+namespace WebProje2.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult cart()
+    {
+        return View();
+    }
+
+    public IActionResult checkout()
+    {
+        return View();
+    }
+
+    public IActionResult shop()
+    {
+        return View();
+    }
+
+    public IActionResult productdetails()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebProje2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
